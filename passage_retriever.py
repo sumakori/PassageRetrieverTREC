6aff0270fa458e3bf21c7539d1511089e3e579bc
Suma Kori <s.kori@tu-braunschweig.de>
2019-06-30 11:10:55 +0200

    Retrieved paragraphs will be written to a separate file

--- passage_retriever.py
+++ passage_retriever.py
@@ -5,9 +5,12 @@ import argparse
 class PassageReteiver:
     def __init__(self, csv, path):
         self.fieldnames = ['TOPIC', 'PMID', 'OFFSET', 'LENGTH', 'Total_PMID', 'Location']
+        self.fieldnames_para = ['TOPIC', 'PMID', 'OFFSET', 'LENGTH', 'Total_PMID', 'Paragraph']
         self.list = []
         self.csv = csv
         self.path = path
+        self.paragraphs = []
+        self.csv_paragraph = ' RelevanceDataset_paragraphs.csv'
 
     def retrive(self, topic, pmid, offset, length, total_pmid, location):
         html_file = self.path + '\\' + location + '\\' + pmid
@@ -16,16 +19,34 @@ class PassageReteiver:
             print(len('Retrieving data from ' + html_file + ' with length ' + str(length) +
                       ' offset ' + str(offset)) * '*')
             
+            paragraph = ''
             with open(html_file) as string:
                 print (string.seek(offset))
                 while string.tell() < offset+length:
-                    print (string.readline(),end='')
-            #print(string.seek([offset:length+offset]))
-            #print(len(string))
-
+                    paragraph += (string.readline())
+                    #print (string.readline(),end='')
+                print(paragraph)
+            list = [topic, pmid.replace('.html',''), offset, length, total_pmid,
+                    paragraph.replace('\"','')]
+            self.paragraphs.append(list)
         else:
             print('File does not exist %s' % (html_file))
-        
+
+    def write_retrieved_paragraphs(self, list):
+        with open(self.csv_paragraph, 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames_para, delimiter = ',',
+                                    escapechar=' ', quoting=csv.QUOTE_MINIMAL)
+            writer.writeheader()
+            for item in list:
+                #paragraph = item[5].replace('\"','')
+                #print (pmid)
+                #print (paragraph)
+                #paragraph = paragraph.strip()
+                writer.writerow({'TOPIC': item[0], 'PMID': item[1].replace('.html', ''),
+                                'OFFSET' : item[2], 'LENGTH': item[3],
+                                'Total_PMID': item[4], 'Paragraph': item[5]})
+    
+
     def read_csv(self):    
         with open(self.csv) as csvfile:
             csvreader = csv.DictReader(csvfile, fieldnames=self.fieldnames, delimiter=',')
@@ -35,22 +56,24 @@ class PassageReteiver:
             row = next(csvreader)
             count = 0
             for row in csvreader:
-                if count < 2:
-                    topic = row['TOPIC']
-                    pmid = row['PMID']
-                    offset = row['OFFSET']
-                    length = row['LENGTH']
-                    total_pmid = row['Total_PMID']
-                    location = row['Location']
-                    list = [topic, pmid, offset, length, total_pmid, location]
-                    self.list.append(list)
-                    count += 1
+                #if count < 5:
+                topic = row['TOPIC']
+                
+                pmid = row['PMID']
+                offset = row['OFFSET']
+                length = row['LENGTH']
+                total_pmid = row['Total_PMID']
+                location = row['Location']
+                list = [topic, pmid, offset, length, total_pmid, location]
+                self.list.append(list)
+                    #count += 1
 
     def run(self):
         self.read_csv()
         for item in self.list:
             self.retrive(item[0], (item[1] + '.html'), int(item[2]), int(item[3]), item[4], item[5])
-
+        print ('len of lists %s paragraphs %s' % (len(self.list), len(self.paragraphs)))
+        self.write_retrieved_paragraphs(self.paragraphs)
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--dataset", help="A valid dataset")
 parser.add_argument("-p", "--path", help="A valid location to journal")
